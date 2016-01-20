81e1be993332054ff201cbf9fb226906944799ad
Mathieu <mathieu0709@gmail.com>
2016-01-20 15:42:40 +0100

    première maquette, champs username,...

--- client/js/main.js
+++ client/js/main.js
@@ -1,2 +1,14 @@
-function main() {
-}
+// Module principale
+var main = angular.module("main", ["chatIrc"]);
+// Dépendance du module principale
+var chatIrc = angular.module("chatIrc", []);
+// Controlleur du module chatIrc
+chatIrc.controller("fieldsController", function($scope) {
+  $scope.infos = {};
+  $scope.showTextField = function(user) {
+    $scope.infos = angular.copy(user);
+    console.log($scope.infos);
+    $scope.showTypeMsg = true;
+    $scope.showForm = true;
+  }
+});
