6d9f2651d6a695c6b2005b53093c67f5dd78cc8c
Mathieu <mathieu0709@gmail.com>
2016-01-21 11:14:23 +0100

    Informations du user ajouté dans un Array, forms bloqués

--- client/js/main.js
+++ client/js/main.js
@@ -4,11 +4,21 @@ var main = angular.module("main", ["chatIrc"]);
 var chatIrc = angular.module("chatIrc", []);
 // Controlleur du module chatIrc
 chatIrc.controller("fieldsController", function($scope) {
-  $scope.infos = {};
-  $scope.showTextField = function(user) {
-    $scope.infos = angular.copy(user);
-    console.log($scope.infos);
-    $scope.showTypeMsg = true;
-    $scope.showForm = true;
+  $scope.infos = []; // Tableau vide destiné à recevoir les infos d'indentification de l'user
+  $scope.showTextField = function(event) { // Fonction lors de l'envoi du formulaire d'identification
+    $scope.infos.push({ // Ajout des infos renseignés par l'user dans le tableau $scope.infos
+      nickname: $scope.user.nickname,
+      username: $scope.user.username,
+      realname: $scope.user.realname,
+      password: $scope.user.password,
+      saloon: $scope.user.joinCommand,
+      command: $scope.user.joinChannel
+    });
+    //console.log($scope.infos);
+    $scope.showForm = true; // Lors de l'envoi du 1er form, on le cache et n affiche le second formulaire
+    event.preventDefault(); // Bloque l'envoi du formulaire
+  };
+  $scope.sendText = function(event) { // Fonction lors de l'envoi du formulaire de message
+    event.preventDefault(); // Bloque l'envoi du formulaire
   }
 });
