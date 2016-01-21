5d91c06183944f7bbd805c294169b887e765e245
Julien00859 <Julien00859@gmail.com>
2016-01-21 03:47:15 +0100

    Ajout des requetes NAMES et TOPIC

--- client/js/IRC.js
+++ client/js/IRC.js
@@ -1,16 +1,36 @@
-var IRC = function IRC(nickname, password, channels, commands) {
+var IRC = function IRC(nickname, password, channels, commands, callback) {
     // Varable d'instance
     var self = this;
     self.socket = io();
-    self.nickname = new String(nickname).valueOf();
-    self.channels = new Array().concat(channels);
-    self.commands = new Array().concat(commands);
-    self.connected = false;
+    self.callbacks = [];
 
-    self.send = function send(e, command) {
+    // Envoyer une commande en brute sur le réseau IRC
+    self.sendCommand = function sendCommand(command) {
         console.log("> " + command);
-        self.socket.emit(e, command)
+        self.socket.emit("command", command);
     }
+
+    // Envoyer un message sur un salon où à quelqu'un
+    self.sendMessage = function sendMessage(channel, message) {
+        self.sendCommand("PRIVMSG " + channel + " :" + message);
+    }
+
+    // Envoyer une requête pour récupérer la liste des utilisateurs d'un salon
+    self.sendNamesQuery = function sendNamesQuery(channel, callback) {
+        console.log("> NAMES " + channel);
+        var callbackId = self.callbacks.length;
+        self.callbacks.push(callback);
+        self.socket.emit("names", channels, callbackId);
+    }
+
+    // Envoyer une requête pour récupérer le topic d'un salon
+    self.sendTopicQuery = function sendTopicQuery(channel, callback) {
+        console.log("> TOPIC " + channel);
+        var callbackId = self.callbacks.length;
+        self.callbacks.push(callback);
+        self.socket.emit("topic", channel, callbackId);
+    }
+
     // Envoie d'une requête d'enregistrement au serveur node
     self.socket.emit("register", nickname, password);
 
@@ -25,16 +45,18 @@ var IRC = function IRC(nickname, password, channels, commands) {
             if (!self.connected) { // Si on a pas encore reçu le MOTD
                 if (line.indexOf("End of message of the day") > -1) {
                     self.connected = true; // Ok on est connecté !
-                    self.channels.forEach(function(chan) self.send("command", "JOIN " + chan)); // On rejoint les salons
-                    self.commands.forEach(function(cmd) self.send("command", cmd)); // On envoie les commandes
+                    channels.forEach(function(chan) self.sendCommand("JOIN " + chan)); // On rejoint les salons
+                    commands.forEach(function(cmd) self.sendCommand(cmd)); // On envoie les commandes
+                    delete channels;
+                    delete commands;
                     break; // On passe les autres messages du MOTD
                 }
             } else { // Sinon si on est en phase de connexion normal
-                linex = line.split(" "); // On récupère chaque mot de la ligne
+                var linex = line.split(" "); // On récupère chaque mot de la ligne
 
                 // Si le 1e mot est PING, on renvoit le PONG correspondant
-                if (linex[0] == "PING") self.send("PONG " + linex[1] + "\r\n");
-                else { // Sinon on regarde chaque 2e mot (le 1e étant alors :<nickname>!<hostname>@<mask>)
+                if (linex[0] == "PING") self.sendCommand("PONG " + linex[1] + "\r\n");
+                else if (linex.length >= 2) { // Sinon on regarde chaque 2e mot (le 1e étant alors :<nickname>!<hostname>@<mask>)
                     switch (linex[1]) {
                         case "PRIVMSG":
                             // :Julien00859!Julien@host-85-201-171-39.dynamic.voo.be PRIVMSG #Dev :Hello world :D
@@ -42,15 +64,35 @@ var IRC = function IRC(nickname, password, channels, commands) {
                             var sender = linex[0].slice(1, linex[0].indexOf("!"));
                             var channel = linex[2];
                             var msg = linex.slice(3).join(" ").slice(1);
-// ==>                      Appeler une fonction du controlleur.
+                            break;
                     }
                 }
             }
         };
     });
-}
 
-// À déplacer dans le controlleur et changer les variables par défaut
+    self.socket.on("topic", function(messages, callbackId){
+        // :127.0.0.1 332 Julien008 #Dev :Hello world
+        //                               "^^^^^^^^^^^"
+        // :127.0.0.1 333 Julien008 #Dev Julien008!Julien@host-85-201-171-39.dynamic.voo.be 1453335926
+
+        var topic = messages.split("\r\n")[0].split(" ").slice(4).join(" ").slice(1);
+        self.callbacks[callbackId](topic);
+        delete self.callbacks[callbackId];
+    });
+
+    self.socket.on("names", function(messages, callbackId) {
+        // :127.0.0.1 353 Julien008 = #Dev :@Julien008 @MrRobot %Mathieu Jean-Kevin
+        //                                 [^^^^^^^^^^,^^^^^^^^,^^^^^^^^,^^^^^^^^^^]
+        // :127.0.0.1 366 Julien008 #Dev :End of /NAMES list.
+
+        var names = messages.split("\r\n")[0].split(" ").slice(5).join(" ").slice(1).split(" ");
+        self.callbacks[callbackId](names);
+        delete self.callbacks[callbackId];
+    });
+}
+var irc;
+// À déplacer dans le controleur et changer les variables par défaut
 function connect() {
     console.log("Connecting to IRC");
     irc = new IRC("WebIRC", "", ["#Dev"], []);
--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ socketio.on("connection", function(socket){
     socket.on("register", function(nickname, password) {
         socket.irc.server = new net.Socket();
         socket.irc.registred = false;
+        socket.irc.callbacks = {"names": {}, "topîc": {}};
+        socket.irc.query = ""; // Ceci est dégueu, pardon, ayez pitié de moi
 
         console.log("Connecting new client to IRC")
         socket.irc.server.connect(config.IRCPort, config.IRCHost, function(){
@@ -36,13 +38,39 @@ socketio.on("connection", function(socket){
 
         socket.irc.server.on("data", function(buffer){
             msg = new String(buffer).valueOf()
-            console.log(msg);
-            socket.emit("IRCMessage", msg);
+
+            if (socket.irc.query != "") { // pardon pardon pardon
+                var channel = msg.split("\r\n")[1].split(" ")[3];
+
+                if (channel in socket.irc.callbacks[socket.irc.query]) {
+                     socket.emit("names", msg, socket.irc.callbacks[socket.irc.query][channel]);
+                     delete socket.irc.callbacks[socket.irc.query][channel]
+                }
+                else socket.emit("names", msg);
+
+                socket.irc.query = "";
+
+            } else {
+                socket.emit("IRCMessage", msg);
+            }
         });
 
         socket.on("command", function(cmd){
-            socket.irc.server.write(cmd + "\r\n")
+            socket.irc.server.write(cmd + "\r\n");
         });
+
+        socket.on("topic", function(channel, callbackId){
+            if (typeof callbackId != "undefined") socket.irc.callbacks.topic[channel] = callbackId;
+            socket.irc.query = "topic"; // Ne me jugez pas par pitié
+            socket.irc.server.write("TOPIC " + channel + "\r\n");
+        });
+
+        socket.on("names", function(channel, callbackId){
+            if (typeof callbackId != "undefined") socket.irc.callbacks.names[channel] = callbackId;
+            socket.irc.query = "names"; // Vraiment je sais que c'est dégueu :'(
+            socket.irc.server.write("NAMES " + channel + "\r\n");
+        });
+
         socket.on('disconnect', function(){
             console.log('user disconnected');
             socket.irc.server.end("QUIT :Page closed");
